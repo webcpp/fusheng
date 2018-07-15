b66a0c5e1a44629aa71360f253b8eb0f64ccad2b
centos <centos@localhost>
2018-07-15 17:44:49 +0800

    update

---
+++ fusheng/fusheng.cpp
@@ -0,0 +1,108 @@
+//#include <mongols/tcp_server.hpp>
+//#include <mongols/tcp_threading_server.hpp>
+//#include <mongols/http_server.hpp>
+#include <mongols/ws_server.hpp>
+#include <unistd.h>
+#include <fstream>
+#include "mongols/lib/json11.hpp"
+#include "mongols/util.hpp"
+
+
+#define PID_FILE                "fusheng.pid"
+#define CONFIG_FILE             "config.json"
+
+int main(int, char**) {
+    daemon(1, 0);
+    std::string line, config_json, err;
+    std::ifstream f(CONFIG_FILE);
+
+    if (f.is_open()) {
+        while (std::getline(f, line)) {
+            config_json.append(line);
+        }
+
+        f.close();
+
+        json11::Json config = json11::Json::parse(config_json, err);
+        if (err.empty()) {
+            std::string host;
+            int port, timeout, buffer_size;
+            host = config["host"].string_value();
+            port = config["port"].int_value();
+            timeout = config["timeout"].int_value();
+            buffer_size = config["buffer_size"].int_value();
+            mongols::ws_server server(host, port, timeout, buffer_size);
+            {
+                std::ofstream pid_file(PID_FILE);
+                pid_file << getpid();
+            }
+            server.run();
+            remove(PID_FILE);
+        }
+    }
+
+
+
+    //    int port = 9999;
+    //    const char* host = "127.0.0.1";
+    //    mongols::ws_server server(host, port);
+    //    	auto f=[](const std::string& input
+    //                , bool& keepalive
+    //                , bool& send_to_other
+    //                , std::pair<size_t, size_t>& g_u_id
+    //                , mongols::tcp_server::filter_handler_function& send_to_other_filter){
+    //    			keepalive = KEEPALIVE_CONNECTION;
+    //    			send_to_other=true;
+    //    			return input;
+    //    	};
+    //    server.run(f);
+
+}
+
+
+/*
+int main(int,char**)
+{
+        auto f=[](const std::string& input
+                , bool& send_to_other
+                , std::pair<size_t, size_t>& g_u_id
+                , mongols::tcp_server::filter_handler_function& send_to_other_filter){
+                                        send_to_other=true;
+                                        return std::make_pair(input,KEEPALIVE_CONNECTION);
+                                };
+        int port=9999;
+        const char* host="127.0.0.1";
+	
+        mongols::tcp_threading_server
+        //mongols::tcp_server
+
+        server(host,port);
+        server.run(f);
+
+}
+ */
+
+
+/*
+int main(int,char**)
+{
+        auto f=[](const mongols::request&){
+                return true;
+        };
+        auto g=[](const mongols::request& req,mongols::response& res){
+                res.content=std::move("hello,world");
+                res.status=200;
+        };
+        int port=9999;
+        const char* host="127.0.0.1";
+        mongols::http_server server(host,port);
+        server.run(f,g);
+}
+ */
+
+
+
+
+
+
+
--- public_html/assets/config.js
+++ public_html/assets/config.js
@@ -1,66 +1,142 @@
 $(function () {
     $('a[title]').tooltip();
 
-    $.getJSON('http://ip-api.com/json/?callback=?', function (result) {
+    var quill = new Quill('#editor-container', {
+        modules: {
+            formula: true,
+            syntax: true,
+            toolbar: '#toolbar-container'
+        },
+        placeholder: 'To be a good man!',
+        theme: 'snow'
+    });
 
+    var toast = new PomeloToast();
+
+    $.getJSON('http://ip-api.com/json/?callback=?', function (result) {
         var user_name = prompt("请输入您的名字", "小蝶");
         if (user_name == null) {
             user_name = '小蝶';
         }
         var address = result.country + '/' + result.regionName + '/' + result.city + '/' + result.query + '/' + user_name;
 
-        function add_msg(str, p) {
-            try {
-                var data = JSON.parse(str);
-                var msg = $('<div class="text-' + p + ' fusheng rcorners"></div>').append("<span>" + data.name + "</span>" + ' said:<br>' + '<p>' + data.message + '</p>');
-                var id = $('#myTab li.active a').attr('data-original-title');
-                $('#' + id).append(msg);
-                $('.tab-content').animate({scrollTop: $('.tab-pane').height()}, 30);
-            } catch (err) {
-                console.log('server:' + str + '\nclient:' + err);
+        function add_msg(str) {
+            var data = {};
+            var tmp = str.split("\r\n");
+            var n = 0;
+            for (var i = 0; i < tmp.length; ++i) {
+                var cur = tmp[i];
+                var pos = cur.indexOf(": ");
+                if (pos != -1) {
+                    var k = cur.substr(0, pos), v = cur.substr(pos + 2, cur.length - 2 - pos);
+                    if (k == 'name') {
+                        data.name = v;
+                        ++n;
+                    } else if (k == 'message') {
+                        data.message = v;
+                        ++n;
+                    } else if (k == 'room') {
+                        data.room = v;
+                        ++n;
+                    }
+                }
+                if (n == 3) {
+                    break;
+                }
+            }
+            if (typeof data.name == 'undefined' || typeof data.message == 'undefined') {
+                data.name = '无名氏';
+                data.message = str;
+            }
+            if (data.name.indexOf(address) == -1) {
+                p = 'left';
+            } else {
+                p = 'right';
+            }
+            var msg = $('<div class="text-' + p + ' fusheng panel panel-default"></div>').append("<div class='panel-body'><span>"
+                    + data.name + "</span>" + ' said:<br>' + '<p>' + (data.message) + '</p></div>');
+            var cur_id = $('#myTab li.active a').attr('data-original-title');
+            if (typeof data.room == 'undefined') {
+                data.room = cur_id;
+            }
+            var id = data.room;
+            $('#' + id).append(msg);
+            $('.tab-content').animate({scrollTop: $('.tab-pane').height()}, 30);
+            $('pre code').each(function (i, block) {
+                hljs.highlightBlock(block);
+            });
+            if (cur_id !== id) {
+                toast.show({
+
+                    // 'error', 'warning', 'success'
+                    // 'white', 'blue'
+                    type: 'success',
+
+                    // toast message
+                    text: id + '有新消息',
+
+                    // default: 3000
+                    time: 3000 // 5 seconds
+
+                });
             }
         }
 
-        var ws = new ReconnectingWebSocket('ws://127.0.0.1:9999');
+        var ws = new ReconnectingWebSocket('ws://127.0.0.1:9999/');
+//        var ws = new WebSocket('ws://127.0.0.1:9999/');
 
         ws.onopen = function ()
         {
-            var data = {name: address, message: 'connected.'};
-            add_msg(JSON.stringify(data), 'right');
+            var data = '';
+            data = data.concat("name: ").concat(address).concat("\r\n").concat("message: ").concat('connected.\r\n');
+            add_msg(data);
         };
 
         ws.onmessage = function (evt)
         {
-            if (evt.data.indexOf(address) == -1) {
-                add_msg(evt.data, 'left');
-            } else {
-                add_msg(evt.data, 'right');
-            }
+            add_msg(evt.data);
 
         };
 
-        ws.onclose = function ()
+        ws.onclose = function (evt)
         {
+
         };
 
+        ws.onerror = function (evt) {
+            toast.show({
 
+                // 'error', 'warning', 'success'
+                // 'white', 'blue'
+                type: 'error',
+
+                // toast message
+                text: 'error',
+
+                // default: 3000
+                time: 3000 // 5 seconds
+
+            });
+        }
 
         var filter_reg = new RegExp('<([a-zA-Z])+.*/?>(.*</([a-zA-Z])+>)?', 'gi');
 
         $('#submit').click(function () {
-            var str = $.trim($("#editor")[0].value).replace(filter_reg, '');
-            if (str.length > 140) {
+//            var str = $.trim($("#editor")[0].value).replace(filter_reg, '');
+            var str = quill.root.innerHTML;
+            if (str.length > 204800) {
                 alert('Too long.');
             } else if (str.length > 0) {
-                var data = {};
-                data.gid = 0;
-                data.uid = 0;
-                data.gfilter = [];
-                data.ufilter = [];
-                data.name = address;
-                data.message = str;
-                var json = JSON.stringify(data);
-                ws.send(json);
+                var data = '';
+                data = data.concat("uid: 0\r\n")
+                        .concat("gid: 0\r\n")
+                        .concat("ufilter: \r\n")
+                        .concat("gfilter: \r\n")
+                        .concat("message: ").concat(str).concat("\r\n")
+                        .concat("name: ").concat(address).concat("\r\n")
+                        .concat("room: ").concat($('#myTab li.active a').attr('data-original-title')).concat('\r\n');
+
+                ws.send(data);
             } else {
                 alert('error message format.');
             }
