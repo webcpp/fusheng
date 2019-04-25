cfdee53ca2529a270ef3a633f54b3327a150cf15
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2019-04-25 12:34:22 +0800

    update

--- public_html/assets/config.js
+++ public_html/assets/config.js
@@ -115,13 +115,15 @@ $(function () {
         create_response(data)
 
         // if (ip_geo == null) {
-        //     $.GET('https://api.ip.sb/geoip/' + data.ip, function (ret) {
-        //         ip_geo = ret.country + ret.region + ret.city
-        //         data.ip = ip_geo
-        //         create_response(data)
-        //     }).fail(function () {
-        //         create_response(data)
-        //     })
+            $.GET('https://api.ip.sb/geoip/' + data.ip, function (ret) {
+                ip_geo = ret.country + ret.region + ret.city
+                data.ip = ip_geo
+                console.log(ip_geo)
+                // create_response(data)
+            }).fail(function () {
+                console.log('get ip_geo error')
+                // create_response(data)
+            })
         // } else {
         //     data.ip = ip_geo;
         //     create_response(data)
